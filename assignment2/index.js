36b32fb356d8802902c4be08f803a27744e3b7bf
Yashsangwan2002 <yashsangwan02.in@gmai.com>
2024-01-22 23:58:45 +0530

    saving file

--- assignment2/index.js
+++ assignment2/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app=express();
 const jwt= require("jsonwebtoken");
 const jwtpass= "yash123";
+const mongoose= require("mongoose");
+mongoose.connect("mongodb+srv://yashindia981:6WKUhf2xnRZpJ8dL@cluster0.qimj1yz.mongodb.net/")
+
+//defing a schema for data validation before putting data into database
+const User = mongoose.model('users',{
+    name: String, password: String
+})
 const limit=5;
 
 app.use(express.json());
@@ -12,42 +19,42 @@ let temp=0;
 let errors=0;
 
 //users data
-const users=[{
-    name:"yash",
-    pass:"123",
-    requests:0,
-},
-{
-    name:"khushi",
-    pass:"123",
-    requests:0,
-},
-];
+// const users=[{
+//     name:"yash",
+//     pass:"123",
+//     requests:0,
+// },
+// {
+//     name:"khushi",
+//     pass:"123",
+//     requests:0,
+// },
+// ];
 
-function userExists(username,pass){
-    for(let i=0;i<users.length;i++){
-        if(users[i].name==username&&users[i].pass==pass){
-            return true;
-        }
-    };
-    return false;
+// function userExists(username,pass){
+//     for(let i=0;i<users.length;i++){
+//         if(users[i].name==username&&users[i].pass==pass){
+//             return true;
+//         }
+//     };
+//     return false;
 
-};
+// };
 //create middlewares here
-function requestCounter(req,res,next){
-    totalRequests++;
-    const name=req.body.username;
-    for(let it of users){
-        if(it.name==name){
-            temp=it.requests;
-            if(++it.requests==limit){
-                res.status(403).json({msg:"too many requests! sign in again"});
-            };
+// function requestCounter(req,res,next){
+//     totalRequests++;
+//     const name=req.body.username;
+//     for(let it of users){
+//         if(it.name==name){
+//             temp=it.requests;
+//             if(++it.requests>limit){
+//                 res.status(403).json({msg:"too many requests! sign in again"});
+//             };
                 
-        };
-    };
-    next();
-}
+//         };
+//     };
+//     next();
+// }
 
 
 function authenticate(req,res,next){
@@ -61,11 +68,11 @@ function authenticate(req,res,next){
 app.get("/signin",(req,res)=>{
     const username =req.body.username;
     const pass=req.body.pass;
-    if(!userExists(username,pass)){
-        return res.status(403).json({
-            msg:"you don't have access here",
-        });
-    };
+    // if(!userExists(username,pass)){
+    //     return res.status(403).json({
+    //         msg:"you don't have access here",
+    //     });
+    // };
     for(let it of users){
         if(it.name==username){
             it.requests=0;
@@ -78,10 +85,20 @@ app.get("/signin",(req,res)=>{
                                 token: token});
 });
 
-app.get("/signup",(req,res)=>{
-    const kidneys =req.body.kidneys;
-    const kidlength=kidneys.length;
-    res.send("you have "+ kidlength +" kidneys");
+app.get("/signup",async (req,res)=>{
+    const username =req.body.username;
+    const password=req.body.pass;
+
+    const userExist= await User.findOne({name: username});
+    if(userExist){
+        return res.status(400).send("user already exists");
+    }
+    const user= new User({
+        name:username,
+        password:pass
+    });
+    user.save();
+    res.send("user created succesfully");
 });
 
 app.get("/calculate",authenticate,requestCounter,(req,res)=>{
